a838319a738bba1f528e9f65034b8dfc12e50dc5
xtaci <daniel820313@gmail.com>
2024-07-08 01:19:40 +0800

    fix test

--- dppk_test.go
+++ dppk_test.go
@@ -11,15 +11,14 @@ func TestDPPK(t *testing.T) {
 	dppk, err := GenerateKey(5)
 	assert.Nil(t, err)
 
-	secret := []byte("W")
+	secret := []byte("hello quantum")
 	Ps, Qs, err := dppk.Encrypt(&dppk.PublicKey, secret)
 	assert.Nil(t, err)
-	t.Log("Ps:", Ps)
-	t.Log("Qs:", Qs)
 	t.Log("secret:", string(secret))
 
 	x1, x2, err := dppk.Decrypt(Ps, Qs)
-	t.Log(string(x1.Bytes()), string(x2.Bytes()))
+	t.Log("x1:", string(x1.Bytes()))
+	t.Log("x2:", string(x2.Bytes()))
 
 	equal := bytes.Equal(secret, x1.Bytes()) || bytes.Equal(secret, x2.Bytes())
 	assert.True(t, equal)
