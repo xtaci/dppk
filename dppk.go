871c4794dcc51dc8da40daf048f788479c780503
xtaci <daniel820313@gmail.com>
2024-07-13 11:47:58 +0800

    add a intro in file header

--- dppk.go
+++ dppk.go
@@ -1,3 +1,22 @@
+// Package dppk implements the DPPK protocol.
+//
+// The ancient Vieta’s formulas reveal the relationships
+// between coefficients of an nth-degree polynomial and its roots. It is
+// surprisingly found that there exists a hidden secret for a potential
+// public key exchange: decoupling the product of all roots or constant
+// term from summations of root products or coefficients of a polynomial
+// to establish a keypair. The proposed deterministic polynomial public
+// key algorithm or DPPK is built on the fact that a polynomial cannot be
+// factorized without its constant term.
+//
+// DPPK allows the keypair generator to combine a base polynomial,
+// eliminable during the decryption, with two solvable polynomials
+// and creates two entangled polynomials. Two coefficient vectors of the
+// entangled polynomials form a public key, and their constant terms,
+// together with the two solvable polynomials, form the private key.
+// By only publishing coefficients of polynomials without their constant
+// terms, we greatly restrict polynomial factoring techniques for the private
+// key extraction.
 package dppk
 
 import (
