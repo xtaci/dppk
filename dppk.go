992c488253d073e878de0f8a495a46b2e87fede0
xtaci <daniel820313@gmail.com>
2024-07-27 14:49:53 +0800

    add license and seperate the package intro to pkg.go

--- dppk.go
+++ dppk.go
@@ -1,22 +1,18 @@
-// Package dppk implements the Deterministic Polynomial Public Key (DPPK) algorithm.
+// # Copyright (c) 2024 xtaci
 //
-// The ancient Vieta’s formulas reveal the relationships
-// between coefficients of an nth-degree polynomial and its roots. It is
-// surprisingly found that there exists a hidden secret for a potential
-// public key exchange: decoupling the product of all roots or constant
-// term from summations of root products or coefficients of a polynomial
-// to establish a keypair. The proposed deterministic polynomial public
-// key algorithm or DPPK is built on the fact that a polynomial cannot be
-// factorized without its constant term.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// DPPK allows the keypair generator to combine a base polynomial,
-// eliminable during the decryption, with two solvable polynomials
-// and creates two entangled polynomials. Two coefficient vectors of the
-// entangled polynomials form a public key, and their constant terms,
-// together with the two solvable polynomials, form the private key.
-// By only publishing coefficients of polynomials without their constant
-// terms, we greatly restrict polynomial factoring techniques for the private
-// key extraction.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package dppk
 
 import (
